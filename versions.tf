97f5b1f940ca8d1cbd01eacd06e2b73d7cb6d0f3
Ignacio Anaya <nacho@checklyhq.com>
2022-03-17 17:21:37 -0300

    chore: upgrade tf sdk and refactor dev build

--- versions.tf
+++ versions.tf
@@ -1,23 +1,18 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = "= 1.1.7"
   required_providers {
     checkly = {
       # dev/checkly/checkly is used for development only,
       # if you're using checkly provider you'll need to follow
       # installation guide instrctions found in README
       source  = "dev/checkly/checkly"
-      version = "0.0.2"
+      version = "0.0.0-canary"
     }
   }
 }
 
-variable "checkly_api_key" {
-}
-
-variable "checkly_account_id" {
-}
-
 provider "checkly" {
-  api_key = var.checkly_api_key
-  account_id = var.checkly_account_id
+  api_key = "cu_cc4010980bd541788ed81ef31790f951"
+  account_id = "4c0b0f96-840d-4eec-b36a-02fa4da5619d"
+  api_url="http://localhost:3000"
 }
